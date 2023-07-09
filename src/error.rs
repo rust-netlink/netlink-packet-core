2c88cc3a8004505f6c3054637f63cb408c99cdf2
Gris Ge <fge@redhat.com>
2023-07-09 15:07:47 +0800

    Revert "Revert "Merge `NetlinkPayload::{Ack,Error}`""
    This reverts commit 16300f56d390b8128fdf37a86b0c0e2bc9d019e3.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-use std::{fmt, io, mem::size_of};
+use std::{fmt, io, mem::size_of, num::NonZeroI32};
 
 use byteorder::{ByteOrder, NativeEndian};
 use netlink_packet_utils::DecodeError;
@@ -46,10 +46,14 @@ impl<T: AsRef<[u8]>> ErrorBuffer<T> {
         }
     }
 
-    /// Return the error code
-    pub fn code(&self) -> i32 {
+    /// Return the error code.
+    ///
+    /// Returns `None` when there is no error to report (the message is an ACK),
+    /// or a `Some(e)` if there is a non-zero error code `e` to report (the
+    /// message is a NACK).
+    pub fn code(&self) -> Option<NonZeroI32> {
         let data = self.buffer.as_ref();
-        NativeEndian::read_i32(&data[CODE])
+        NonZeroI32::new(NativeEndian::read_i32(&data[CODE]))
     }
 }
 
@@ -77,14 +81,28 @@ impl<T: AsRef<[u8]> + AsMut<[u8]>> ErrorBuffer<T> {
     }
 }
 
+/// An `NLMSG_ERROR` message.
+///
+/// Per [RFC 3549 section 2.3.2.2], this message carries the return code for a
+/// request which will indicate either success (an ACK) or failure (a NACK).
+///
+/// [RFC 3549 section 2.3.2.2]: https://datatracker.ietf.org/doc/html/rfc3549#section-2.3.2.2
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
 #[non_exhaustive]
 pub struct ErrorMessage {
-    pub code: i32,
+    /// The error code.
+    ///
+    /// Holds `None` when there is no error to report (the message is an ACK),
+    /// or a `Some(e)` if there is a non-zero error code `e` to report (the
+    /// message is a NACK).
+    ///
+    /// See [Netlink message types] for details.
+    ///
+    /// [Netlink message types]: https://kernel.org/doc/html/next/userspace-api/netlink/intro.html#netlink-message-types
+    pub code: Option<NonZeroI32>,
+    /// The original request's header.
     pub header: Vec<u8>,
 }
-
-pub type AckMessage = ErrorMessage;
 
 impl Emitable for ErrorMessage {
     fn buffer_len(&self) -> usize {
@@ -92,7 +110,7 @@ impl Emitable for ErrorMessage {
     }
     fn emit(&self, buffer: &mut [u8]) {
         let mut buffer = ErrorBuffer::new(buffer);
-        buffer.set_code(self.code);
+        buffer.set_code(self.raw_code());
         buffer.payload_mut().copy_from_slice(&self.header)
     }
 }
@@ -119,13 +137,18 @@ impl<'buffer, T: AsRef<[u8]> + 'buffer> Parseable<ErrorBuffer<&'buffer T>>
 }
 
 impl ErrorMessage {
+    /// Returns the raw error code.
+    pub fn raw_code(&self) -> i32 {
+        self.code.map_or(0, NonZeroI32::get)
+    }
+
     /// According to [`netlink(7)`](https://linux.die.net/man/7/netlink)
     /// the `NLMSG_ERROR` return Negative errno or 0 for acknowledgements.
     ///
     /// convert into [`std::io::Error`](https://doc.rust-lang.org/std/io/struct.Error.html)
     /// using the absolute value from errno code
     pub fn to_io(&self) -> io::Error {
-        io::Error::from_raw_os_error(self.code.abs())
+        io::Error::from_raw_os_error(self.raw_code().abs())
     }
 }
 
@@ -149,7 +172,7 @@ mod tests {
     fn into_io_error() {
         let io_err = io::Error::from_raw_os_error(95);
         let err_msg = ErrorMessage {
-            code: -95,
+            code: NonZeroI32::new(-95),
             header: vec![],
         };
 
@@ -157,5 +180,41 @@ mod tests {
 
         assert_eq!(err_msg.to_string(), io_err.to_string());
         assert_eq!(to_io.raw_os_error(), io_err.raw_os_error());
+    }
+
+    #[test]
+    fn parse_ack() {
+        let bytes = vec![0, 0, 0, 0];
+        let msg = ErrorBuffer::new_checked(&bytes)
+            .and_then(|buf| ErrorMessage::parse(&buf))
+            .expect("failed to parse NLMSG_ERROR");
+        assert_eq!(
+            ErrorMessage {
+                code: None,
+                header: Vec::new()
+            },
+            msg
+        );
+        assert_eq!(msg.raw_code(), 0);
+    }
+
+    #[test]
+    fn parse_nack() {
+        // SAFETY: value is non-zero.
+        const ERROR_CODE: NonZeroI32 =
+            unsafe { NonZeroI32::new_unchecked(-1234) };
+        let mut bytes = vec![0, 0, 0, 0];
+        NativeEndian::write_i32(&mut bytes, ERROR_CODE.get());
+        let msg = ErrorBuffer::new_checked(&bytes)
+            .and_then(|buf| ErrorMessage::parse(&buf))
+            .expect("failed to parse NLMSG_ERROR");
+        assert_eq!(
+            ErrorMessage {
+                code: Some(ERROR_CODE),
+                header: Vec::new()
+            },
+            msg
+        );
+        assert_eq!(msg.raw_code(), ERROR_CODE.get());
     }
 }
