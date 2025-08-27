cc6bf089ed9bc74b37df52cfaa331ec5b6ae9c15
Gris Ge <fge@redhat.com>
2025-08-27 19:08:10 +0800

    macro: Fix `buffer!` macro
    Signed-off-by: Gris Ge <fge@redhat.com>

--- src/macros.rs
+++ src/macros.rs
@@ -21,20 +21,17 @@ macro_rules! getter {
     };
     ($name:ident, u16, $offset:expr) => {
         pub fn $name(&self) -> u16 {
-            use $crate::byteorder::{ByteOrder, NativeEndian};
-            NativeEndian::read_u16(&self.buffer.as_ref()[$offset])
+            $crate::parse_u16(&self.buffer.as_ref()[$offset]).unwrap()
         }
     };
     ($name:ident, u32, $offset:expr) => {
         pub fn $name(&self) -> u32 {
-            use $crate::byteorder::{ByteOrder, NativeEndian};
-            NativeEndian::read_u32(&self.buffer.as_ref()[$offset])
+            $crate::parse_u32(&self.buffer.as_ref()[$offset]).unwrap()
         }
     };
     ($name:ident, u64, $offset:expr) => {
         pub fn $name(&self) -> u64 {
-            use $crate::byteorder::{ByteOrder, NativeEndian};
-            NativeEndian::read_u64(&self.buffer.as_ref()[$offset])
+            $crate::parse_u64(&self.buffer.as_ref()[$offset]).unwrap()
         }
     };
     ($name:ident, i8, $offset:expr) => {
@@ -44,20 +41,17 @@ macro_rules! getter {
     };
     ($name:ident, i16, $offset:expr) => {
         pub fn $name(&self) -> i16 {
-            use $crate::byteorder::{ByteOrder, NativeEndian};
-            NativeEndian::read_i16(&self.buffer.as_ref()[$offset])
+            $crate::parse_i16(&self.buffer.as_ref()[$offset]).unwrap()
         }
     };
     ($name:ident, i32, $offset:expr) => {
         pub fn $name(&self) -> i32 {
-            use $crate::byteorder::{ByteOrder, NativeEndian};
-            NativeEndian::read_i32(&self.buffer.as_ref()[$offset])
+            $crate::parse_i32(&self.buffer.as_ref()[$offset]).unwrap()
         }
     };
     ($name:ident, i64, $offset:expr) => {
         pub fn $name(&self) -> i64 {
-            use $crate::byteorder::{ByteOrder, NativeEndian};
-            NativeEndian::read_i64(&self.buffer.as_ref()[$offset])
+            $crate::parse_i64(&self.buffer.as_ref()[$offset]).unwrap()
         }
     };
 }
@@ -66,7 +60,7 @@ macro_rules! getter {
 macro_rules! setter {
     ($buffer: ident, $name:ident, slice, $offset:expr) => {
         impl<'a, T: AsRef<[u8]> + AsMut<[u8]> + ?Sized> $buffer<&'a mut T> {
-            $crate::pastey::item! {
+            $crate::paste! {
                 pub fn [<$name _mut>](&mut self) -> &mut [u8] {
                     &mut self.buffer.as_mut()[$offset]
                 }
@@ -79,64 +73,58 @@ macro_rules! setter {
         }
     };
     ($name:ident, u8, $offset:expr) => {
-        $crate::pastey::item! {
+        $crate::paste! {
             pub fn [<set_ $name>](&mut self, value: u8) {
                 self.buffer.as_mut()[$offset] = value;
             }
         }
     };
     ($name:ident, u16, $offset:expr) => {
-        $crate::pastey::item! {
+        $crate::paste! {
             pub fn [<set_ $name>](&mut self, value: u16) {
-                use $crate::byteorder::{ByteOrder, NativeEndian};
-                NativeEndian::write_u16(&mut self.buffer.as_mut()[$offset], value)
+                $crate::emit_u16(&mut self.buffer.as_mut()[$offset], value).unwrap()
             }
         }
     };
     ($name:ident, u32, $offset:expr) => {
-        $crate::pastey::item! {
+        $crate::paste! {
             pub fn [<set_ $name>](&mut self, value: u32) {
-                use $crate::byteorder::{ByteOrder, NativeEndian};
-                NativeEndian::write_u32(&mut self.buffer.as_mut()[$offset], value)
+                $crate::emit_u32(&mut self.buffer.as_mut()[$offset], value).unwrap()
             }
         }
     };
     ($name:ident, u64, $offset:expr) => {
-        $crate::pastey::item! {
+        $crate::paste! {
             pub fn [<set_ $name>](&mut self, value: u64) {
-                use $crate::byteorder::{ByteOrder, NativeEndian};
-                NativeEndian::write_u64(&mut self.buffer.as_mut()[$offset], value)
+                $crate::emit_u64(&mut self.buffer.as_mut()[$offset], value).unwrap()
             }
         }
     };
     ($name:ident, i8, $offset:expr) => {
-        $crate::pastey::item! {
+        $crate::paste! {
             pub fn [<set_ $name>](&mut self, value: i8) {
                 self.buffer.as_mut()[$offset] = value;
             }
         }
     };
     ($name:ident, i16, $offset:expr) => {
-        $crate::pastey::item! {
+        $crate::paste! {
             pub fn [<set_ $name>](&mut self, value: i16) {
-                use $crate::byteorder::{ByteOrder, NativeEndian};
-                NativeEndian::write_i16(&mut self.buffer.as_mut()[$offset], value)
+                $crate::emit_i16(&mut self.buffer.as_mut()[$offset], value).unwrap()
             }
         }
     };
     ($name:ident, i32, $offset:expr) => {
-        $crate::pastey::item! {
+        $crate::paste! {
             pub fn [<set_ $name>](&mut self, value: i32) {
-                use $crate::byteorder::{ByteOrder, NativeEndian};
-                NativeEndian::write_i32(&mut self.buffer.as_mut()[$offset], value)
+                $crate::emit_i32(&mut self.buffer.as_mut()[$offset], value).unwrap()
             }
         }
     };
     ($name:ident, i64, $offset:expr) => {
-        $crate::pastey::item! {
+        $crate::paste! {
             pub fn [<set_ $name>](&mut self, value: i64) {
-                use $crate::byteorder::{ByteOrder, NativeEndian};
-                NativeEndian::write_i64(&mut self.buffer.as_mut()[$offset], value)
+                $crate::emit_i64(&mut self.buffer.as_mut()[$offset], value).unwrap()
             }
         }
     };
@@ -145,24 +133,24 @@ macro_rules! setter {
 #[macro_export]
 macro_rules! buffer {
     ($name:ident($buffer_len:expr) { $($field:ident : ($ty:tt, $offset:expr)),* $(,)? }) => {
-        buffer!($name { $($field: ($ty, $offset),)* });
-        buffer_check_length!($name($buffer_len));
+        $crate::buffer!($name { $($field: ($ty, $offset),)* });
+        $crate::buffer_check_length!($name($buffer_len));
     };
 
     ($name:ident { $($field:ident : ($ty:tt, $offset:expr)),* $(,)? }) => {
-        buffer_common!($name);
+        $crate::buffer_common!($name);
         fields!($name {
             $($field: ($ty, $offset),)*
         });
     };
 
     ($name:ident, $buffer_len:expr) => {
-        buffer_common!($name);
-        buffer_check_length!($name($buffer_len));
+        $crate::buffer_common!($name);
+        $crate::buffer_check_length!($name($buffer_len));
     };
 
     ($name:ident) => {
-        buffer_common!($name);
+        $crate::buffer_common!($name);
     };
 }
 
@@ -170,11 +158,11 @@ macro_rules! buffer {
 macro_rules! fields {
     ($buffer:ident { $($name:ident : ($ty:tt, $offset:expr)),* $(,)? }) => {
         $(
-            getter!($buffer, $name, $ty, $offset);
+            $crate::getter!($buffer, $name, $ty, $offset);
         )*
 
             $(
-                setter!($buffer, $name, $ty, $offset);
+                $crate::setter!($buffer, $name, $ty, $offset);
             )*
     }
 }
@@ -183,20 +171,20 @@ macro_rules! fields {
 macro_rules! buffer_check_length {
     ($name:ident($buffer_len:expr)) => {
         impl<T: AsRef<[u8]>> $name<T> {
-            pub fn new_checked(buffer: T) -> Result<Self, DecodeError> {
+            pub fn new_checked(buffer: T) -> Result<Self, $crate::DecodeError> {
                 let packet = Self::new(buffer);
                 packet.check_buffer_length()?;
                 Ok(packet)
             }
 
-            fn check_buffer_length(&self) -> Result<(), DecodeError> {
+            fn check_buffer_length(&self) -> Result<(), $crate::DecodeError> {
                 let len = self.buffer.as_ref().len();
                 if len < $buffer_len {
-                    Err(DecodeError::InvalidBuffer {
-                        name: stringify!($name),
-                        received: len,
-                        minimum_length: $buffer_len,
-                    })
+                    Err($crate::DecodeError::invalid_buffer(
+                        stringify!($name),
+                        len,
+                        $buffer_len,
+                    ))
                 } else {
                     Ok(())
                 }
